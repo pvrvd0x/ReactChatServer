246fe4e4b955cb1e88396fa11fdba2dfefa78679
Master <d3m35s1ah@protonmail.com>
2019-10-12 10:54:31 +0300

    Fix bug with broken message render

--- src/controllers/Messages.ts
+++ src/controllers/Messages.ts
@@ -31,15 +31,17 @@ class MessageController {
         message
             .save()
             .then((obj: any) => {
-                obj.populate('dialog', (err: any, message: any) => {
+                obj.populate(['dialog', 'user'], (err: any, message: any) => {
                     if (err) {
                         return res
                                 .status(500)
-                                .json(err);
+                                .json({
+                                    status: 'error',
+                                    message: err
+                                });
                     }
-                })
 
-                DialogsModel.findOneAndUpdate(
+                    DialogsModel.findOneAndUpdate(
                     { _id: postData.dialog },
                     { lastMessage: message._id },
                     { upsert: true },
@@ -56,15 +58,48 @@ class MessageController {
 
                 this.io.emit('MESSAGES:NEW_MESSAGE', message);
             });
-        }
+        })
+    }
 
     public delete = (req: Request, res:Response) => {
         const id = req.params.id;
 
         MessageModel.findOneAndDelete({_id: id})
-            .then(dialog => {
-                if (dialog) {
-                    res.json({ message: "Message Deleted" });
+            .then((data: any) => {
+                if (data.dialog) {
+                    MessageModel.find(
+                        {dialog: data.dialog},
+                        (err, messages) => {
+                            if (err) {
+                                res.status(404).json({
+                                    status: 'error',
+                                    message: err
+                                })
+                            }
+
+                            const lastMessage = messages[messages.length - 1];
+
+                            DialogsModel.findOneAndUpdate(
+                                {_id: data.dialog},
+                                { lastMessage: lastMessage._id},
+                                (err) => {
+                                    if (err) {
+                                        res.status(500).json({
+                                            status: 'error',
+                                            message: err,
+                                        });
+                                    }
+
+                                    res.json({
+                                        status: 'success',
+                                        data
+                                    });
+
+                                    this.io.emit('MESSAGES:MESSAGE_DELETED');
+                                }
+                            )
+                        }
+                    )
                 }
             })
             .catch(err => res.json(err))
