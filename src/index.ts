500fdcf39e6f40d151098cc0792491f5de594f7c
Master <d3m35s1ah@protonmail.com>
2019-10-13 20:54:42 +0300

    Change port for Heroku

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,6 @@ configureRoutes(app, io);
 
 configureSockets(io);
 
-server.listen(1337, function() {
+server.listen(process.env.PORT || 1337, function() {
     console.log(`Listening to your speech nigga on port ${process.env.PORT}`);
 });
