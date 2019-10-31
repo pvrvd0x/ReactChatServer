56ba6b9665945c67c35b930910ae725167b33c3b
d3m35s1ah <d3m35s1ah@protonmail.com>
2019-10-31 10:18:21 +0300

    Add latest frontend build

---
+++ build/precache-manifest.5a56849af800e9d65927322ab02f38f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e91d43bc5a335cc03acd8cfab26e4ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "794dcae6a240eddc6b25",
+    "url": "/static/css/2.e5b92c76.chunk.css"
+  },
+  {
+    "revision": "9063ceafb0e4c50ffc57",
+    "url": "/static/css/main.3b4b95bc.chunk.css"
+  },
+  {
+    "revision": "794dcae6a240eddc6b25",
+    "url": "/static/js/2.ee5124ad.chunk.js"
+  },
+  {
+    "revision": "9063ceafb0e4c50ffc57",
+    "url": "/static/js/main.bf84cf38.chunk.js"
+  },
+  {
+    "revision": "cd6183a92790f1f410ee",
+    "url": "/static/js/runtime~main.ffb341a7.js"
+  },
+  {
+    "revision": "05bd65e91a86050dcf88c80b6ab7ae63",
+    "url": "/static/media/checked.05bd65e9.svg"
+  },
+  {
+    "revision": "030b4c1a96f35d267e4a02649b932ffd",
+    "url": "/static/media/pause.030b4c1a.svg"
+  },
+  {
+    "revision": "a1d839aa671cce7043ee4a3e4d7ddd50",
+    "url": "/static/media/play.a1d839aa.svg"
+  },
+  {
+    "revision": "70aff14a3f79e1695f1b188cf498e1df",
+    "url": "/static/media/unchecked.70aff14a.svg"
+  },
+  {
+    "revision": "004b528dbaa9001f57a656f2e031bed6",
+    "url": "/static/media/wave.004b528d.svg"
+  },
+  {
+    "revision": "8e9ed25225499724c2fdaa58aa856feb",
+    "url": "/static/media/waveBlack.8e9ed252.svg"
+  }
+]);
